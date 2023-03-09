165ad8533262663022426e2addc75035eade98b5
SamBorisov <88675952+SamBorisov@users.noreply.github.com>
2023-03-09 16:44:39 +0200

    Create split.js

---
+++ ES6/split.js
@@ -0,0 +1,7 @@
+
+//splits the text between what we say (" ") spacebar
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+let word = myArray[1];
+//print the second item into arr (are)
