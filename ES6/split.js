d96036dd0d8702b3ab6defb0672472460817d9ef
SamBorisov <88675952+SamBorisov@users.noreply.github.com>
2023-03-09 16:46:24 +0200

    Update split.js

--- ES6/split.js
+++ ES6/split.js
@@ -4,4 +4,9 @@
 let text = "How are you doing today?";
 const myArray = text.split(" ");
 let word = myArray[1];
+console.log(word);
 //print the second item into arr (are)
+
+//splits only 3 items (how,are,you)
+const myArray2 = text.split(" ", 3);
+console.log(myArray2);
